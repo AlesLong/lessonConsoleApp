0d971c191a57a04be3cdc0678f153c8bf21ac3f3
Yevheniy <yevbes@gmail.com>
2022-08-11 23:13:56 +0200

    Добавьте файлы проекта.

---
+++ lessonConsoleApp/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace lessonConsoleApp
+{
+    internal class Employee
+    {
+        private string name; 
+        private string surname;
+        private int age;
+        private int salary;
+        bool isWorking;
+        char sex;
+
+        public Employee(string name,
+                        string surname,
+                        int age,
+                        int salary,
+                        bool isWorking,
+                        char sex)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Salary = salary;
+            this.IsWorking = isWorking;
+            this.Sex = sex;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int Age { get => age; set => age = value; }
+        public int Salary { get => salary; set => salary = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+        public char Sex { get => sex; set => sex = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Employee employee &&
+                   Name == employee.Name &&
+                   Surname == employee.Surname &&
+                   Age == employee.Age &&
+                   Salary == employee.Salary &&
+                   IsWorking == employee.IsWorking &&
+                   Sex == employee.Sex;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1338273066;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            hashCode = hashCode * -1521134295 + Age.GetHashCode();
+            hashCode = hashCode * -1521134295 + Salary.GetHashCode();
+            hashCode = hashCode * -1521134295 + IsWorking.GetHashCode();
+            hashCode = hashCode * -1521134295 + Sex.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return "{name : "+ name + "}{ surname : " + surname + "}{ age : " + age + "}{ salary : " 
+                + salary + "}{ isWorking : " + isWorking + "}{ sex : " + sex +"}";
+        }
+    }
+}
---
+++ lessonConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessonConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Vasya", "Pupkin", 23, 9999, true, 'm');
+            Employee emp2 = new Employee("Sveta", "Sokolova", 30, 3333, false, 'f');
+            Employee emp3 = new Employee("Dave", "Mustaine", 61, 22222, true,'m');
+            Employee emp4 = new Employee("Dave", "Elefson", 23, 1234, false, 'm') ;
+            Employee emp5 = new Employee("Vasya", "Pupkin", 23, 9999, true, 'f');
+
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(emp1);
+            employeeList.Add(emp2);
+            employeeList.Add(emp3);
+            employeeList.Add(emp4);
+            employeeList.Add(emp5);
+
+            List<Employee> filtredList = new List<Employee>();
+
+            filtredList.AddRange(employeeList.Where(el => (el.Name == "Vasya"&&el.IsWorking == true)));
+
+            filtredList.ForEach(el => Console.WriteLine(el));
+
+            List<Employee> filtredList2 = new List<Employee>();
+
+            filtredList2.AddRange(employeeList.Where(el => el.Name == "Dave" && el.Sex == 'm'));
+
+            filtredList2.ForEach(el => Console.WriteLine(el));
+
+            Console.ReadLine();
+        }
+    }
+}
